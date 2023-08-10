2140353db28702f97d17fd87f13f3d2a84fad484
mehmet korur <m.korur.7071@gmail.com>
2023-08-10 13:20:50 +0200

    day34_inheritance

---
+++ src/day34_inheritance/Corolla.java
@@ -0,0 +1,8 @@
+package day34_inheritance;
+
+public class Corolla extends BToyota{
+    String uretimYeri = "Sakarya / Turkiye";
+    String model = "Corolla";
+    String aku = "Inci aku";
+    String sanziman = "Cvt";
+}
