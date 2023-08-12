a4c861959d36632f5a4b830dd868bb2839116587
mehmet korur <m.korur.7071@gmail.com>
2023-08-12 13:21:36 +0200

    day36_inheritance_Datatypekul

---
+++ src/day36_inheritance_Datatypekul/CAvciKuslar.java
@@ -0,0 +1,9 @@
+package day36_inheritance_Datatypekul;
+
+public class CAvciKuslar extends BKuslar {
+    String hareket= "ucarlar";
+    String beslenme= "et yerler";
+    String pence= "pencelidir";
+    String gaga= "sivri gagali";
+
+}
