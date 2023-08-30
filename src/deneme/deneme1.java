8de6049a19b10a8ab782925b6fc32eb533126869
mehmet korur <m.korur.7071@gmail.com>
2023-08-30 13:35:28 +0200

    day46 overridin

---
+++ src/deneme/deneme1.java
@@ -0,0 +1,15 @@
+package deneme;
+
+import java.util.Scanner;
+
+public class deneme1 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("lutfen bir numarasi giriniz");
+        int ay =scanner.nextInt();
+        switch (ay){
+            case 1:
+                System.out.println("");
+        }
+    }
+}
