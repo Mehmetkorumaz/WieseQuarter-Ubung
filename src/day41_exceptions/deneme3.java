3f20ae54ca5d262a22c4dee8f04670d97b853e06
mehmet korur <m.korur.7071@gmail.com>
2023-09-04 11:20:27 +0200

    mapler

--- src/day41_exceptions/deneme3.java
+++ src/day41_exceptions/deneme3.java
@@ -1,5 +1,8 @@
 package day41_exceptions;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class deneme3 {
     public static void main(String[] args) {
         //kullanicidan toplanmak uzere tamsayilar alin
@@ -11,5 +14,26 @@ public class deneme3 {
         int sayac=0;
         boolean sayiIste=true;
         String tercih="";
+        Scanner scanner=new Scanner(System.in);
+
+        while (sayiIste){
+
+            System.out.println("lutfen toplanmak uzere tamsayilar giriniz\nBitirmek icin q ya basiniz");
+
+            try { // normal girislerde calisacak kodlar
+                sayi= scanner.nextInt();
+                toplam+=sayi;
+                sayac++;
+            } catch (InputMismatchException e) {// Exceptions ta calisacak kodlar
+                tercih=scanner.next();// kullanici yanlis giris yaptiginda veya Q ya bastiginda
+                if (tercih.equalsIgnoreCase("q")){
+                    sayiIste=false; // veya direk break yazila bilir
+                }else{
+                    System.out.println("toplamak icin tamsayi girmeniz gerekiyor");
+                }
+
+            }
+        }
+        System.out.println("toplam : "+ sayac+"  ,adet sayi girdiniz. Toplamlari :  "+toplam);
     }
 }
